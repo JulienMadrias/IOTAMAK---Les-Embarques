46f36afa94b97da264b1334fb8c8c6acc1facbbe
bmt_dev <bmtdev11@gmail.com>
2022-03-14 15:26:46 +0100

    ser/deser

--- code/sma/sma_amas.py
+++ code/sma/sma_amas.py
@@ -1,6 +1,8 @@
 from pyAmakCore.classes.amas import Amas
 from sma_agent import SimpleAgent
 from sma_environnement import SimpleEnvironment
+import numpy as np
+import pickle
 
 
 class SimpleAmas(Amas):
@@ -26,3 +28,21 @@ class SimpleAmas(Amas):
         ps[0].add_neighbour(ps[len(ps) - 1])
         ps[len(ps) - 1].add_neighbour(ps[0])
         self.add_agents(ps)
+
+        # distribute agents
+        # if we have 10 agents and 2 connected raspberry, we'll have 5 on each
+        # let's assume we have 2 rasp
+        nb_raspberry = 2
+
+        to_send = [list(elt) for elt in np.array_split(ps, nb_raspberry)]
+
+        # serialisation
+        # for i, elt in enumerate(to_send):
+        #     with open(f'rasp_{i}', 'wb') as f:
+        #         pickle.dump(elt, f)
+
+        # deserialisation
+        with open('rasp_0', 'rb') as f:
+            r = pickle.load(f)
+            for el in r:
+                print(el._SimpleAgent__state)
